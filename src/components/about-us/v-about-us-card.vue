3e0233dc3f8f39b2f7e8d2086c4946f00bb49022
Евгений Давыдов <davydov.e@office.lan>
2023-09-28 19:20:28 +0300

    added some blocks as components

---
+++ src/components/about-us/v-about-us-card.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="about-us__founder-card about-us-founder-card">
+        <img
+                :src="require('../../assets/images/home/' + block.image.src)"
+                :alt="block.image.alt"
+                class="boxes-card-icon__img"
+            >
+        <div class="about-us-founder-card__column">
+            <h4 class="about-us-founder-card__title"> {{ block.title }} </h4>
+            <span class="about-us-founder-card__text"> {{ block.subtitle }} </span>
+            <v-button class="about-us-founder-card__button"
+                :title="block.button.title"
+                :isGradient="block.button.isGradient"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+
+import vButton from '../v-button.vue';
+
+export default {
+    name: 'v-about-us-card',
+    components: {
+        vButton
+    },
+    props: {
+        block: {
+            type: Object,
+            default() {
+                return {};
+            }
+        }
+    }
+}
+
+</script>
